3dcb98acabe2a4a4c6f22e7af30379f74bbcd264
Diego <diegoteixeirapereira@hotmail.com>
2021-01-13 17:01:49 -0300

    alteração no arquivo classe Contact.java adicionado getter e setter provisorios

--- spring-boot-thymeleaf-crud/src/main/java/br/com/java/domain/Contact.java
+++ spring-boot-thymeleaf-crud/src/main/java/br/com/java/domain/Contact.java
@@ -64,4 +64,78 @@ public class Contact implements Serializable {
 	
 	@Column
 	private String note;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getAddress3() {
+		return address3;
+	}
+
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+
+	public String getPostalcode() {
+		return postalcode;
+	}
+
+	public void setPostalcode(String postalcode) {
+		this.postalcode = postalcode;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	
 }
